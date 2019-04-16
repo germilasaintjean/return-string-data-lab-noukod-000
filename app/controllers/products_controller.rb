74c794e804d1ec38ff84f086c25218b5b215f5fd
Saint-Jean Germila <germila@noukod.com>
2019-04-16 14:32:53 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,30 +1,40 @@
 class ProductsController < ApplicationController
+  def new
+    @product=Product.new
+  end
+
   def index
-      @products = Product.all
-    end
-#
-    def inventory
-      product = Product.find(params[:id])
-      render plain: product.inventory > 0 ? true : false
-    end
+    @products=Product.all
+  end
 
-    def description
-      product = Product.find(params[:id])
-      render plain: product.description
-    end
-
-    def new
-      @product = Product.new
-    end
-
-    def create
-      Product.create(product_params)
+  def create
+    @product=Product.new(product_params)
+    if @product.save
       redirect_to products_path
+    else
+      redirect_to new_product_path
     end
+  end
 
-    private
-
-    def product_params
-      params.require(:product).permit(:name, :description, :inventory, :price)
+  def description
+    @product=Product.find_by(id:params[:id])
+    if @product
+      render plain: @product.description
+    else
+      redirect "/products"
     end
+  end
+
+  def inventory
+    @product=Product.find_by(id:params[:id])
+    if @product
+      render plain: @product.inventory > 0
+    else
+      redirect "/products"
+    end
+  end
+
+  def product_params
+    params.require(:product).permit(:name,:price,:inventory,:description)
+  end
 end
