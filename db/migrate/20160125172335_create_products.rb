eb609055994fac8bd01d111bce16ba22a7beebbb
Saint-Jean Germila <germila@noukod.com>
2019-04-16 14:14:31 +0000

    Automatically backed up by Learn

--- db/migrate/20160125172335_create_products.rb
+++ db/migrate/20160125172335_create_products.rb
@@ -4,6 +4,7 @@ class CreateProducts < ActiveRecord::Migration
       t.string :name
       t.integer :price
       t.text :description
+      t.integer :inventory
       t.timestamps null: false
     end
   end
